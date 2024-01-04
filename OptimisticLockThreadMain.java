3d04ab2c679628267a71fbca243899dddad384e1
Swati-Tech09 <80823399+Swati-Tech09@users.noreply.github.com>
2024-01-04 22:40:33 +0530

    Optimistic lock

--- OptimisticLockThreadMain.java
+++ OptimisticLockThreadMain.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class OptimisticLockThreadMain {
+public class OptimisticLockThreadMain {
+    public static void main(String[] args) {
+        SharedOptimisticLock sharedOptimisticLock = new SharedOptimisticLock();
+
+        Thread t1 = new Thread(()->{
+            sharedOptimisticLock.producer();});
+        Thread t2 = new Thread(()->{
+            sharedOptimisticLock.consumer();});
+        t1.setName("Thread1");
+        t1.start();
+        t2.setName("Thread2");
+        t2.start();
+    }
 }
--- SharedOptimisticLock.java
+++ SharedOptimisticLock.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class SharedOptimisticLock {
+import java.util.concurrent.locks.StampedLock;
+
+public class SharedOptimisticLock {
+    int a =10;
+    StampedLock lock = new StampedLock();
+
+    public void producer()
+    {
+        System.out.println("Optimistic lock read -" + Thread.currentThread().getName());
+        long stamp = lock.tryOptimisticRead();
+        try {
+            a=11;
+            Thread.sleep(3000);
+            if(lock.validate(stamp))
+            {
+                System.out.println("Updated successfully - " +Thread.currentThread().getName());
+            }
+            else
+            {
+                System.out.println("rollback of write -" +Thread.currentThread().getName());
+                a=10;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void consumer()
+    {
+        long stamp = lock.writeLock();
+        System.out.println("Write lock - "+Thread.currentThread().getName());
+        try
+        {
+         a=9;
+        }
+        finally {
+            lock.unlockWrite(stamp);
+            System.out.println("write lock released by "+Thread.currentThread().getName());
+
+        }
+
+    }
+
+
 }
